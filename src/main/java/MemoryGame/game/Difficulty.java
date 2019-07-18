585b7d8930c2c797f2ad63de00985db504bff434
mnl-laterite <e8163264128@gmail.com>
2019-07-18 08:23:21 +0300

    add difficulty contents

--- src/main/java/MemoryGame/game/Difficulty.java
+++ src/main/java/MemoryGame/game/Difficulty.java
@@ -1,5 +1,15 @@
 package MemoryGame.game;
 
 public enum Difficulty {
-  EASY, DEFAULT, HARD;
+  EASY(4, 3),
+  DEFAULT(8, 4),
+  HARD(12, 5);
+
+  private Difficulty(int pairsTotal, int gamePlayMapDepth) {
+    this.pairsTotal = pairsTotal;
+    this.gamePlayMapDepth = gamePlayMapDepth;
+  }
+
+  public final int pairsTotal;
+  public final int gamePlayMapDepth;
 }
